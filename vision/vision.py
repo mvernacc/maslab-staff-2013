6d8bfa0ac235457b0f6e23b9661e1979e9f5558e
Matthew Vernacchia <mvernacc@mit.edu>
2013-01-16 00:55:28 -0500

    Updated Vision Code from Mock 1

--- vision/vision.py
+++ vision/vision.py
@@ -1,4 +1,4 @@
-# MASLab Team 4
+ # MASLab Team 4
 # 8 Jan 2013
 # Finding A Red Ball in an Image using OpenCV cv2 package
 
@@ -17,6 +17,8 @@ class Vision:
             - debug - Whether the program produces debuggin output. true --> pro                gram draws images and prints output, false --> program does not
                 draw images or print output (faster)"""
         self.capture = cv2.VideoCapture(1)
+        self.capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320)
+        self.capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
         self.image = None # the latest image
         self.red_side = red_side
         self.debug = debug # a boolean - true=debug mode, draws images
@@ -40,7 +42,7 @@ class Vision:
         # Identify the red portions of the image. Since red exists where the H numbers
         # HSV loop around from 180 to 0, this is a bit tricky
         hue = (170, 10) # range of hue (red is centered at 0 or 180)
-        sat = (100, 255) # range of saturation
+        sat = (150, 255) # range of saturation
         val = (5, 250)  # range of value
         red_hsv_min_1 = np.array([hue[0], sat[0], val[0]], np.uint8)
         red_hsv_max_1 = np.array([180, sat[1], val[1]],np.uint8)
@@ -75,6 +77,7 @@ class Vision:
         if self.debug: cv2.imshow(self.win_color, ball_color_image)
 # Reduce Noise with morphological operations
         height, width= ball_color_image.shape
+        print width
         morph_size = width/100
         element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (morph_size, morph_size))
         filtered_image = cv2.erode(ball_color_image, element)
@@ -82,9 +85,35 @@ class Vision:
         filtered_image = cv2.dilate(filtered_image, element)
 # Show the noise-reduced image
         if self.debug: cv2.imshow(self.win_color_filt, filtered_image)
+
+	M = cv2.moments(filtered_image)
+	if M['m00'] == 0: return []
+	cx = int(M['m10']/M['m00'])
+	return [Feature(0, 30.0 * (1.0 - 2.0 * cx / width), Feature.BALL)]
+
+	contours, hierarchy = cv2.findContours(filtered_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	feats = []
+	for contour in contours:
+		M = cv2.moments(contour)
+		cx = int(M['m10']/M['m00'])
+		feats.append(Feature(0, 30.0 * (1.0 - 2.0 * cx / width), Feature.BALL))
+	return feats
+
+	cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+
+	contours, hierarchy = cv2.findContours(filtered_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	feats = []
+	for contour in contours:
+		print contour
+		break
+	        # angle = 30.0 * (1.0 - 2.0 * contour[0] / width)
+        	# feats.append(Feature(0, angle, Feature.BALL) )
+	return feats
+
 # Do Canny
         edges = cv2.Canny(filtered_image, 0, 251)
         if self.debug: cv2.imshow(self.win_edges, edges)
+
 # Do Hough Transform 
 #circles = cv2.HoughCircles(edges, cv.CV_HOUGH_GRADIENT, 2, width/50 )
         circles = cv2.HoughCircles(edges, cv.CV_HOUGH_GRADIENT, 4, width/10,\
